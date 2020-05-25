f86562fe830b3e70bf618d1d2cc9bf2979621b4b
kvasov-dev <biz.kvasov@gmail.com>
2020-05-25 21:26:58 +0300

    Models are changed

--- src/main/java/ru/vrn/medsys/entities/dto/UserDto.java
+++ src/main/java/ru/vrn/medsys/entities/dto/UserDto.java
@@ -1,13 +1,16 @@
 package ru.vrn.medsys.entities.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Collection;
+import java.util.Set;
 
 @Data
 public class UserDto {
     private Long id;
     private String name;
     private String email;
-    private Collection<RoleDto> roles;
+    private Set<String> roles;
 }
