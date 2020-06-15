601f783b0b4032f7ffd2cab4037e185c048c6f75
Klishin Ilya <klishin-ilya@mail.ru>
2020-06-16 01:30:45 +0300

    dto swagger doc

--- src/main/java/ru/vrn/medsys/entities/dto/NewUserDto.java
+++ src/main/java/ru/vrn/medsys/entities/dto/NewUserDto.java
@@ -1,13 +1,20 @@
 package ru.vrn.medsys.entities.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.Collection;
 
+@ApiModel(value = "Новый пользователь", description = "Данные нового пользователя")
 @Data
 public class NewUserDto {
+    @ApiModelProperty(notes = "Имя пользователя")
     private String name;
+    @ApiModelProperty(notes = "Email пользователя")
     private String email;
+    @ApiModelProperty(notes = "Пароль")
     private String password;
+    @ApiModelProperty(notes = "Роли пользователя")
     private Collection<RoleDto> roles;
 }
--- src/main/java/ru/vrn/medsys/entities/dto/UserDto.java
+++ src/main/java/ru/vrn/medsys/entities/dto/UserDto.java
@@ -1,5 +1,7 @@
 package ru.vrn.medsys.entities.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,10 +9,15 @@ import lombok.Setter;
 import java.util.Collection;
 import java.util.Set;
 
+@ApiModel(value = "Пользователь", description = "Данные пользователя")
 @Data
 public class UserDto {
+    @ApiModelProperty(notes = "Id пользователя")
     private Long id;
+    @ApiModelProperty(notes = "Имя пользователя")
     private String name;
+    @ApiModelProperty(notes = "Email пользователя")
     private String email;
+    @ApiModelProperty(notes = "Роли пользователя")
     private Set<String> roles;
 }
